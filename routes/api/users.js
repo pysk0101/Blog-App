ed25f505a714d2672c4dd38ebb662424abbcfef1
pysk0101 <kucukaslanmuhammed852@gmail.com>
2024-06-19 17:06:09 +0300

    some fixed

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,12 @@
 import express from "express"
 const router = express.Router()
 
+import verifyJWT from '../../middleware/verifyJWT.js';
 
 
-
-import  {deleteUser,updateUser} from "../../controllers/userController.js"
-
-router.delete("/", deleteUser).put("/", updateUser)
+import  {getUsers, readUser ,deleteUser,updateUser} from "../../controllers/userController.js"
+ 
+router.get("/", verifyJWT ,getUsers ).get("/:id", readUser).delete("/:id", deleteUser).put("/:id", updateUser)
 
 
 export default router
