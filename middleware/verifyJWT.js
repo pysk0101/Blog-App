e8718b08a3cf7e68436c6566baed8b6d2409a10c
pysk0101 <kucukaslanmuhammed852@gmail.com>
2024-06-19 12:10:07 +0300

    fixed

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -7,13 +7,11 @@ const verifyJWT = async (req, res, next) => {
     try {
 
         const token = req.headers["authorization"]?.split(" ")[1];
-
-        console.log(token)
        
-        if (!token) return res.status(403).render("yasaklandın.ejs")
+        if (!token) return res.status(403).send("Pls login first")
 
         jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-            if (err) return res.status(403).send("Token geçersiz puhahahahahahh")
+            if (err) return res.status(403).send("Invalid Token")
             next()
         })
     }
