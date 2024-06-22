37fe530fdc967871611b5e26c66af161b7257d55
pysk0101 <kucukaslanmuhammed852@gmail.com>
2024-06-22 15:54:03 +0300

    updated logout

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -1,18 +1,6 @@
-import Users from '../models/userModel.js' 
-
-
 const logoutController = async (req, res) => {
-    const { refreshToken } = req.body;
-    if (!refreshToken) return res.sendStatus(401);
-
-
-    const foundUser = await  Users.findOne( { refreshToken: refreshToken});
-    if (!foundUser) return res.sendStatus(403);
-
-    
-    foundUser.refreshToken = ""; // refresh token'ı sıfırla
-    
-    await foundUser.save();
+    res.clearCookie("at");
+    res.clearCookie("rt");
     res.redirect("/");
 }
 
