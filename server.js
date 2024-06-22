91e63514cd55f0c7fa6cb715b4aa9e412b32d2a9
pysk0101 <kucukaslanmuhammed852@gmail.com>
2024-06-22 15:58:25 +0300

    a little bit adjustment

--- server.js
+++ server.js
@@ -2,41 +2,41 @@
 import express from 'express';
 import connectDB from './config/db.js';
 import dotenv from "dotenv"
+import cookieParser from 'cookie-parser';
+
 
 const app = express();
 dotenv.config()
 
+
+//Middlewares
+app.set("view engine", "ejs")
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//?Routes
+//Database
+connectDB()
+
+//Routes
 import registerRouter from './routes/register.js';
 import authRouter from './routes/auth.js';
-import refreshRouter from './routes/refresh.js';
 import logoutRouter from './routes/logout.js';
 import userRouter from './routes/api/users.js';
 import pageRouter from './routes/pages/page.js';
 
 
-
-connectDB()
-
-//?APIs
+//APIs
 app.use("/register", registerRouter);
-app.use("/auth", authRouter);
-app.use("/users", userRouter);
-app.use("/refresh", refreshRouter);
+app.use("/login", authRouter);
 app.use("/logout", logoutRouter);
+app.use("/users", userRouter)
 app.use("/", pageRouter);
 
 
 
+const PORT = 5000;
 
-
-
-
-
-const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Sunucu http://localhost:${PORT} adresinde çalışıyor.`);
 });
