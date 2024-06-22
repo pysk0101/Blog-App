b219a60617406b7ad7e85fefb1c52fbe359ed05b
pysk0101 <kucukaslanmuhammed852@gmail.com>
2024-06-22 11:45:47 +0300

    updated refresh token middleware

---
+++ middleware/refreshToken.js
@@ -0,0 +1,25 @@
+
+import jwt from "jsonwebtoken";
+
+
+const refreshTokenController = async (req, res) => {
+    try {
+
+        const refreshToken = req.cookies.rt;
+        if (!refreshToken) return res.sendStatus(401);
+        console.log(refreshToken)
+        jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
+            if (err) return res.sendStatus(403);
+            //decoded= paylod kısmı
+            const token = jwt.sign({ email: decoded.email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.cookie("at", token, { httpOnly: true });
+            console.log(token)
+            console.log("token yenilendi")
+            res.redirect("/users")
+        })
+    } catch (error) {
+        res.status(500).json({ 'message': error.message })
+    }
+}
+
+export default refreshTokenController
