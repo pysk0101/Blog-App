80e0f8faaf1e24d8087a054fd6f3b1ac7068b24e
pysk0101 <kucukaslanmuhammed852@gmail.com>
2024-06-25 23:30:37 +0300

    added blog id to

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -2,23 +2,24 @@ import Users from '../models/userModel.js'
 
 const createBlog = async (req, res) => {
     try {
-        const  username  = req.params.username
+        const username = req.params.username
         const user = await Users.findOne({ username: username })
         console.log(req.body)
         if (!user) return res.status(400).json({ message: `User ${username} not found` })
-        
 
-        const { title, content} = req.body
+
+        const { title, content } = req.body
         const newBlog = {
+            id: user.blogs.length + 1,
             title,
             content,
         }
-        
+
         user.blogs.push(newBlog)
         user.updatedAt = Date.now()
 
         await user.save()
-        
+
         res.redirect(`/myprofile/@${user.username}`)
 
 
@@ -28,4 +29,29 @@ const createBlog = async (req, res) => {
     }
 }
 
-export { createBlog }
+const updateBlog = async (req, res) => {
+    try {
+        const { title, content } = req.body
+        
+        const username = req.query.username
+        const id = req.query.id
+
+        const user = await Users.findOne({ username: username })
+        console.log(user)
+        if (!user) return res.status(400).json({ message: `User ${username} not found` })
+
+        const blog = user.blogs.find(blog => blog.id == id)
+        console.log(blog)
+        blog.title= title
+        blog.content = content
+        console.log(blog)
+        
+        await user.save()
+        res.redirect(`/myprofile/@${user.username}`)
+    } catch (error) {
+        res.status(500).json({ message: "An error occurred", error})
+    }
+
+}
+
+export { createBlog, updateBlog }
